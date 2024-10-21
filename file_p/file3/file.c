53f88418f50b65e2b7e2f6c16cb7158559d481d8
BhargavDevi <1si23is012@sit.ac.in>
2024-10-21 23:15:31 +0530

    new

---
+++ file_p/file3/file.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(int argc,char *argv[]){
+    char ch;
+    int count=0;
+    FILE *fp;
+    if (argc!=2)
+    {
+        printf("pass only 2 arguments");
+        return 1;
+    }
+    fp=fopen(argv[1],"r");
+    if(fp==NULL){
+        printf("File not found");
+        return 1;
+    }
+    while ((ch=fgetc(fp))!=EOF)
+    {
+        if (ch=='\n')
+        {
+            count++;
+        }
+        if (count%2==1)
+        {
+            printf("%c",ch);
+
+        }
+        
+    }
+    return 0;
+
+}
