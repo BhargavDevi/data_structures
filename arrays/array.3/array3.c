f7a3b6a63d56bea82645ddc278bffbf7f8a91f54
BhargavDevi <1si23is012@sit.ac.in>
2024-10-22 21:13:43 +0530

    new

--- arrays/array.3/array3.c
+++ arrays/array.3/array3.c
@@ -1,6 +1,5 @@
 // Problem Statement: Given an array of size n, write a program to check if the given array is sorted in (ascending / Increasing / Non-decreasing) order or not. If the array is sorted then return True, Else return False.
 
-
 #include <stdio.h>
 // #include<stdbool. h>
 int arra(int arr[], int n)
@@ -14,17 +13,14 @@ int arra(int arr[], int n)
             {
                 return 0;
             }
-            
-             
-            
         }
     }
-       return 1;
+    return 1;
 }
 int main()
 {
     int arr[] = {3, 4, 6, 7};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    int n = sizeof(arr) / sizeof(arr[0]);
     int c = arra(arr, n);
     if (c == 0)
     {
